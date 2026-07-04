5d382c6a8863f75aeac08f78b76b15a272264c88
Blazing-Jas991 <223303023+Blazing-Jas991@users.noreply.github.com>
2026-07-04 06:10:41 -0700

    add basic percentage calculator function and styling

---
+++ script.js
@@ -0,0 +1,40 @@
+const percentInput = document.querySelector("#percent-input");
+const numberInput = document.querySelector("#number-input");
+const resultOutput = document.querySelector(".result");
+resultOutput.style.display = "none";
+
+function getPercentage() {
+  let percentNum = Number(percentInput.value);
+  let calNumber = Number(numberInput.value);
+  if (isNaN(percentNum) || isNaN(calNumber)) {
+    return;
+  }
+  const calculatedPercent = Math.round(percentNum * (calNumber / 100));
+  if (
+    Number(calculatedPercent) === 0 ||
+    percentInput === "" ||
+    numberInput === ""
+  ) {
+    resultOutput.style.display = "none";
+    return;
+  }
+  resultOutput.textContent = `${percentNum}% of ${calNumber} is: ${calculatedPercent}`;
+}
+
+const calButton = document.querySelector(".calculation-button");
+
+calButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  resultOutput.style.display = "flex";
+  getPercentage();
+  percentInput.value = "";
+  numberInput.value = "";
+});
+
+percentInput.addEventListener("input", () => {
+  resultOutput.style.display = "none";
+});
+
+numberInput.addEventListener("input", () => {
+  resultOutput.style.display = "none";
+});
